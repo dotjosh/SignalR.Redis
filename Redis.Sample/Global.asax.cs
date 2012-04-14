5828743965e2ad33680716f07b11fb65fa213035
David Fowler <davidfowl@gmail.com>
2012-04-14 10:27:47 -0700

    Added sample web app.

---
+++ Redis.Sample/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using SignalR.Redis;
+using System.Configuration;
+
+namespace Redis.Sample
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            // Hook up redis
+            string server = ConfigurationManager.AppSettings["redis.server"];
+            string port = ConfigurationManager.AppSettings["redis.port"];
+            string password = ConfigurationManager.AppSettings["redis.password"];
+
+            SignalR.Global.DependencyResolver.UseRedis(server, Int32.Parse(port), password, "SignalR.Redis.Sample");
+        }
+    }
+}
