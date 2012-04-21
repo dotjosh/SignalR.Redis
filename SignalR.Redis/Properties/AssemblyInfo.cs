e739bf0fb4855e6f1d5835bffa3e12634b8494a3
David Fowler <davidfowl@gmail.com>
2012-04-21 13:19:06 -0700

    Fixed description and author on package and updated targets file.

--- SignalR.Redis/Properties/AssemblyInfo.cs
+++ SignalR.Redis/Properties/AssemblyInfo.cs
@@ -1,8 +1,11 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("SignalR.Redis")]
-[assembly: AssemblyCompany("David Fowler")]
 [assembly: AssemblyProduct("SignalR.Redis")]
+[assembly: AssemblyDescription("Redis message bus implementation for SignalR")]
+
+[assembly: AssemblyCompany("David Fowler, Damian Edwards")]
+[assembly: AssemblyCopyright("Copyright © David Fowler and Damian Edwards 2012")]
 
 [assembly: AssemblyVersion("0.5.0.0")]
 [assembly: AssemblyFileVersion("0.5.0.0")]
