10748d6f52ace523ef0327deb49fb1d3f4abf5b9
David Fowler <davidfowl@gmail.com>
2012-04-21 11:33:20 -0700

    Added build.cmd and some basic versioning.

--- SignalR.Redis/Properties/AssemblyInfo.cs
+++ SignalR.Redis/Properties/AssemblyInfo.cs
@@ -3,4 +3,7 @@
 [assembly: AssemblyTitle("SignalR.Redis")]
 [assembly: AssemblyCompany("David Fowler")]
 [assembly: AssemblyProduct("SignalR.Redis")]
-[assembly: AssemblyVersion("0.4")]
+
+[assembly: AssemblyVersion("0.5.0.0")]
+[assembly: AssemblyFileVersion("0.5.0.0")]
+[assembly: AssemblyInformationalVersion("0.5.0-pre")]
