3c24e11f8ac03e38560a67a44073c84ac8241f84
David Fowler <davidfowl@gmail.com>
2012-05-09 01:47:59 -0700

    Changed the version to 0.1

--- SignalR.Redis/Properties/AssemblyInfo.cs
+++ SignalR.Redis/Properties/AssemblyInfo.cs
@@ -7,6 +7,6 @@
 [assembly: AssemblyCompany("David Fowler, Damian Edwards")]
 [assembly: AssemblyCopyright("Copyright © David Fowler and Damian Edwards 2012")]
 
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.0.0")]
-[assembly: AssemblyInformationalVersion("0.5.0-pre")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0.0")]
