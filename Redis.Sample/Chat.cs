6ffa1d21e549ff52efe3c9293cbf5de623c5af1c
David Fowler <davidfowl@gmail.com>
2012-05-08 23:58:47 -0700

    Updated sample and added disconnect reconnect, messages.

--- Redis.Sample/Chat.cs
+++ Redis.Sample/Chat.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using SignalR.Hubs;
+using System.Threading.Tasks;
 
 namespace Redis.Sample
 {
-    public class Chat : Hub
+    public class Chat : Hub, IConnected, IDisconnect
     {
         public void Send(string message)
         {
             // Call the addMessage method on all clients
             Clients.addMessage(message);
+        }
+
+        public Task Connect()
+        {
+            return Clients.join(Context.ConnectionId);
+        }
+
+        public Task Reconnect(IEnumerable<string> groups)
+        {
+            return Clients.reconnect(Context.ConnectionId);
+        }
+
+        public Task Disconnect()
+        {
+            return Clients.drop(Context.ConnectionId);
         }
     }
 }
